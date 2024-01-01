b779829ad957e8568592385d5c0e1957433b9329
ACER <shoh12102008>
2024-01-01 22:00:20 +0500

    OnBoarding

--- lib/data/utils/app_colors.dart
+++ lib/data/utils/app_colors.dart
@@ -2,4 +2,8 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color kGrey = Colors.grey;
+  static const Color white = Colors.white;
+  ///On bu OnBoardingdagi rang
+  static const Color Onblue = Color(0xFF1877F2);
+  static const Color OnText = Color(0xFF4E4B66);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kakar_news/data/utils/app_routes.dart';
+import 'package:kakar_news/ui/pages/onBoarding_page/onBoarding_page.dart';
 import 'package:kakar_news/ui/pages/splash_page/splash_page.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: SplashPage.routeName,
+      initialRoute: OnBoardingPage.routeName,
       onGenerateRoute: AppRouteUtils.onGenerateRoute,
     );
   }
