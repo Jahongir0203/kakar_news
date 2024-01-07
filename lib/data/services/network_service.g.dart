df2ee56fe9950765726754f4e80eda68d8ac36bf
HP Envy <muxtorova.dilnoza1996@gmail.com>
2024-01-07 20:40:49 +0500

    homePage page

--- lib/data/services/network_service.g.dart
+++ lib/data/services/network_service.g.dart
@@ -53,6 +53,33 @@ class _NetworkService implements NetworkService {
     return value;
   }
 
+  @override
+  Future<NotificationModel> getNotifications(String apiKey) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{r'apiKey': apiKey};
+    final _headers = <String, dynamic>{};
+    final Map<String, dynamic>? _data = null;
+    final _result = await _dio
+        .fetch<Map<String, dynamic>>(_setStreamType<NotificationModel>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/top-headlines/sources',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    final value = NotificationModel.fromJson(_result.data!);
+    return value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
