0478459883cbe3b13e63feab5a4df9aadad666a5
HP Envy <muxtorova.dilnoza1996@gmail.com>
2024-01-08 16:02:54 +0500

    homePage page

--- lib/ui/pages/home_page/all_page.dart
+++ lib/ui/pages/home_page/all_page.dart
@@ -4,6 +4,7 @@ import 'package:dio/dio.dart';
 import '../../../data/blocs/all_bloc/all_bloc.dart';
 
 import '../../../data/services/network_service.dart';
+import '../../../data/utils/app_png.dart';
 
 class AllPage extends StatelessWidget {
   const AllPage({Key? key}) : super(key: key);
@@ -19,26 +20,59 @@ class AllPage extends StatelessWidget {
             return CircularProgressIndicator();
           } else if (state is AllSucsess) {
             return Scaffold(
-              body: Container(
-                child: ListView(
-                  children: [
-                    Column(
-                      children: [
-                        Container(
-                          height: 170,
-                          width: 360,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            image: DecorationImage(
-                              image: NetworkImage("${state.trendingNewsModel.articles?.last.urlToImage ?? ""}"),
-                              fit: BoxFit.fill,
-                            ),
+              body: Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Container(
+                  child: ListView.builder(
+                    itemCount: state.trendingNewsModel.articles?.length ?? 0,
+                    itemBuilder: (context, index) {
+                      var article = state.trendingNewsModel.articles![index];
+
+                      return Column(
+                        children: [
+                          Row(
+                            children: [
+                              Container(
+                                height: 96,
+                                width: 96,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  image: DecorationImage(
+                                    image: NetworkImage("${article.urlToImage ?? ""}"),
+                                    fit: BoxFit.fill,
+                                  ),
+                                ),
+                              ),
+                              Expanded(child: Column(
+                                children: [
+                                  Text("${article.title ?? ""}",style: TextStyle(fontWeight: FontWeight.w400,fontSize: 16),),
+                                  Row(
+                                    children: [
+                                      Image.asset(
+                                        AppPng.kBBC,
+                                        height: 20,
+                                      ),
+                                      Text("${article.source?.name}",style: TextStyle(fontWeight: FontWeight.w600,fontSize: 13),),
+                                      SizedBox(width: 10,),
+                                      Icon(
+                                        Icons.watch_later_outlined,
+                                        size: 12,
+                                      ),
+                                      Text("${article.publishedAt?.substring(0, 10)}",style: TextStyle(fontWeight: FontWeight.w400,fontSize: 13),),
+                                    ],
+                                  ),
+                                ],
+                              )),
+                            ],
                           ),
-                        ),
-                        Text("${state.trendingNewsModel.articles?.first.title ?? ""}"),
-                      ],
-                    ),
-                  ],
+
+                          // Add additional widgets as needed for each article
+                          SizedBox(height: 16), // Add spacing between articles
+                        ],
+                      );
+                    },
+                  ),
+
                 ),
               ),
             );
