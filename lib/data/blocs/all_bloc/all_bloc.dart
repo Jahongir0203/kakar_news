f485156fb11a5d45a0162f9c7d894ed9059c4394
HP Envy <muxtorova.dilnoza1996@gmail.com>
2024-01-08 14:14:27 +0500

    homePage page

--- lib/data/blocs/all_bloc/all_bloc.dart
+++ lib/data/blocs/all_bloc/all_bloc.dart
@@ -3,9 +3,7 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
-
 import '../../models/trending_news_model.dart';
-
 import '../../services/network_service.dart';
 
 part 'all_event.dart';
@@ -14,22 +12,20 @@ part 'all_state.dart';
 class AllBloc extends Bloc<AllEvent, AllState> {
   final NetworkService networkService;
   AllBloc(this.networkService) : super(AllInitial()) {
-
     on<AllLoadedEvent>((event, emit) {
       emit(AllLoading());
-      try{
-        var response= networkService.allNews();
-        emit(AllSucsess(response as TrendingNewsModel));
+     try{
+       var response=networkService.allNews();
 
+       emit(AllSucsess(response as TrendingNewsModel ));
 
-      }
-      catch(e){
-        print(e);
-        emit(AllFailur());
-      }
+     }
+     catch(e){
+       print(e);
+       emit(AllFailur());
+
+     }
 
     });
   }
-
-
 }
